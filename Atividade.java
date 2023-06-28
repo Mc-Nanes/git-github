1c21bd1557e55f9f4f5e889cf3e45598f6fe6aaa
Mc-Nanes <lon@poli.br>
2023-06-27 23:04:32 -0300

    novo módulo

--- Atividade.java
+++ Atividade.java
@@ -1,4 +1,15 @@
 public class Atividade {
     private String ALuno;
     private String Professor;
+
+    public Atividade(String aLuno, String professor) {
+        ALuno = aLuno;
+        Professor = professor;
+    }
+    public void Ensinando(){
+        /*
+         * 
+         */
+    }
 }
+    
